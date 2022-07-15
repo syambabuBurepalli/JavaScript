a30f2eb611edaee690c10925366a17523d99a0f0
syambabuBurepalli <syambabubrs@gmail.com>
2022-07-15 21:07:29 +0530

    First commit

---
+++ para.js
@@ -0,0 +1,19 @@
+
+<html>
+ <body>
+<script>
+function myFunction(){
+    document.getElementById("demo")>inner HTML="Paragraph changed";
+}
+</script>
+</head>
+<body>
+<h2>Demo JavaScript in Heaad</h2>
+<p id="demo">A Paragraph.</p>
+<button type="button"onclick="myFunction()">Try it</button>
+
+</body>
+</html>
+
+
+
---
+++ syam.js
@@ -0,0 +1,2 @@
+console.log("syam babu");
+console.log("ramu");
